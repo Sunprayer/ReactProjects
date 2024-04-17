ce35b2298cd28c2121bcefd10fdc42e6ec0fcbdc
Maksim Cornijs <maksimscornijs@gmail.com>
2024-04-17 19:25:28 +0200

    Installed tailwind finished accordion

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -3,18 +3,50 @@ import data from "./data";
 
 export default function Accordion() {
   const [selected, setSelected] = useState(null);
-  function handleSelection(getCurrentId) {}
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+
+  function handleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
+  function handleMultiSelection(getCurrentId) {
+    let copyMultiple = [...multiple];
+    const findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId);
+
+    if (findIndexOfCurrentId === -1) copyMultiple.push(getCurrentId);
+    else copyMultiple.splice(findIndexOfCurrentId, 1);
+
+    setMultiple(copyMultiple);
+  }
 
   return (
-    <div className="wrapper">
-      <div>
+    <div className="flex justify-center items-center flex-col gap-[20px]">
+      <button
+        onClick={() => setEnableMultiSelection(!enableMultiSelection)}
+        className="border-2 border-slate-900 rounded-lg"
+      >
+        Enable multiselection
+      </button>
+      <div className="w-[500px] justify-center ">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
-            <div>
-              <div onClick={() => handleSelection(dataItem.id)}>
+            <div className="bg-slate-500">
+              <div
+                className="text-white flex justify-between content-center cursor-pointer  "
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSelection(dataItem.id)
+                }
+              >
                 <h3>{dataItem.title}</h3>
                 <span>+</span>
               </div>
+              {selected === dataItem.id ||
+              multiple.indexOf(dataItem.id) !== -1 ? (
+                <div className="h-auto text-white">{dataItem.content}</div>
+              ) : null}
             </div>
           ))
         ) : (
